899a45a0c5d158437c0d8a85fb13f852c4af6bac
3neef <hajiki.326@hotmail.com>
2021-09-24 22:35:50 +0200

    wip

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -1,17 +1,38 @@
 <div>
-    <h3>دبل فك</h3>
-    <div id='map' style='width: 400px; height: 300px;'></div>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h2>My Map</h2>
+                </div>
+                <div class="card-body">
+                    <div id='map' style=' width:100%; height:80vh; '></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    My shit
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
 
 @push('scripts')
 
 <script>
-    mapboxgl.accessToken = 'pk.eyJ1IjoiM25lZWYiLCJhIjoiY2t0eWswbWU5MzQxMTJvbXBxemNva3pkbCJ9.csVXrNlNdFdX8TmFEt4QHw';
+    mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
     var map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11'
+      style: 'mapbox://styles/mapbox/dark-v10'
     });
+    map.addControl(new mapboxgl.NavigationControl());
   </script>
   
     
