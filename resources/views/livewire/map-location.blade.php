4c42f26f99e632ae1aa97f4a07243847f77db8d8
3neef <hajiki.326@hotmail.com>
2021-09-24 23:51:13 +0200

    displying coordinates

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">
+                <div class="card-header bg-dark text-white text-center">
                     <h2>My Map</h2>
                 </div>
                 <div class="card-body">
@@ -12,10 +12,24 @@
         </div>
         <div class="col-md-4">
             <div class="card">
-                <div class="card-header">
+                <div class="card-header bg-dark text-white text-center">
                     My shit
                 </div>
                 <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Longitude</label>
+                                <input  wire:model="long" type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label>Latitude</label>
+                                <input wire:model="lat" type="text" class="form-control">
+                            </div>
+                        </div>
+                    </div>
                     
                 </div>
             </div>
@@ -27,13 +41,29 @@
 @push('scripts')
 
 <script>
-    mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/dark-v10'
-    });
-    map.addControl(new mapboxgl.NavigationControl());
-  </script>
+    document.addEventListener('livewire:load', ()=>{
+        const defaultlocation = [32.48134141930012, 15.64275642504424]
+
+        mapboxgl.accessToken = '{{env("MAPBOX_KEY")}}';
+        var map = new mapboxgl.Map({
+        container: 'map',
+        center: defaultlocation,
+        zoom: 11.15,
+        style: 'mapbox://styles/mapbox/dark-v10'
+        });
+        map.addControl(new mapboxgl.NavigationControl());
+
+
+        map.on('click', (e) => {
+            const maz = e.lngLat.lng
+            const zax = e.lngLat.lat
+
+            @this.long = maz 
+            @this.lat = zax
+        })
+    })
+        
+</script>
   
     
 @endpush
